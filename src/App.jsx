f88b03ee6a6dc4046788b7808b2f1cd03e86192d
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-01-03 07:34:37 +0000

    Add EducationalExperience to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import PersonalDetails from './components/FormInput'
+import { EducationalExperience } from './components/FormInput'
 
 function App() {
   return (
     <>
       <PersonalDetails></PersonalDetails>
+      <EducationalExperience></EducationalExperience>
     </>
   )
 }
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -34,3 +34,45 @@ export default function PersonalDetails () {
         </div>
     )
 }
+
+export function EducationalExperience () {
+    const [school, setSchool] = useState('');
+    const [degree, setDegree] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    function handleSchoolChange(e) {
+        setSchool(e.target.value);
+    }
+    function handleDegreeChange(e) {
+        setDegree(e.target.value);
+    }
+    function handleStartDateChange(e) {
+        setStartDate(e.target.value);
+    }
+    function handleEndDateChange(e) {
+        setEndDate(e.target.value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={e => e.preventDefault()}>
+                <label htmlFor="school">School name: </label>
+                <input type="text" name="school" id="school" placeholder="Enter school / university"
+                value={school} onChange={handleSchoolChange}/>
+
+                <label htmlFor="degree">Degree: </label>
+                <input type="text" name="degree" id="degree" placeholder="Enter degree / field of study"
+                value={degree} onChange={handleDegreeChange}/>
+
+                <label htmlFor="startDate">Start Date: </label>
+                <input type="text" name="startDate" id="startDate" placeholder="Enter Start Date"
+                value={startDate} onChange={handleStartDateChange}/>
+
+                <label htmlFor="endDate">End Date: </label>
+                <input type="text" name="endDate" id="endDate" placeholder="Enter End Date"
+                value={endDate} onChange={handleEndDateChange}/>
+            </form>
+        </div>
+    )
+}
