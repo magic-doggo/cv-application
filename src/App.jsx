bc61846b225a84e1c30bded3ba90ed70a700e223
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-01-21 07:25:50 +0000

    Separate PracticalExperience component and correctly calculate ids will later use the ids as keys for rendering submitted form data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import PersonalDetails, { EducationalExperience, PracticalExperience } from './components/FormInput'
+import PersonalDetails, { EducationalExperience} from './components/FormInput'
+import { PracticalExperience } from './components/PracticalExperience'
 
 function App() {
   return (
---
+++ src/components/PracticalExperience.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+export function PracticalExperience() {
+    const [companyName, setCompanyName] = useState('');
+    const [positionTitle, setPositionTitle] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [description, setDescription] = useState('');
+    const [nextId, setNextId] = useState(0);
+    const [experienceEntries, setExperienceEntries] = useState([]);
+
+    function handleCompanyNameChange(e) {
+        setCompanyName(e.target.value)
+    }
+    function handlePositionTitleChange(e) {
+        setPositionTitle(e.target.value)
+    }
+    function handleStartDateChange(e) {
+        setStartDate(e.target.value);
+    }
+    function handleEndDateChange(e) {
+        setEndDate(e.target.value);
+    }
+    function handleDescriptionChange(e) {
+        setDescription(e.target.value);
+    }
+
+
+
+    function addExperience() {
+        setExperienceEntries([...experienceEntries, {
+            companyName: companyName,
+            positionTitle: positionTitle,
+            startDate: startDate,
+            endDate: endDate,
+            description: description,
+            id: (nextId)
+        }]);
+        setNextId(nextId + 1);
+        console.log(nextId);
+        console.log(experienceEntries);
+    }
+
+
+    return (
+        <div className="add-experience-section">
+            <div>Experience</div>
+            <form onSubmit={e => { e.preventDefault(); addExperience() }}>
+                <label htmlFor="companyName">Company Name: </label>
+                <input type="text" name="companyName" id="companyName" placeholder="Enter Company Name"
+                    value={companyName} onChange={handleCompanyNameChange} />
+
+                <label htmlFor="positionTitle">Position Title: </label>
+                <input type="text" name="positionTitle" id="positionTitle" placeholder="Enter Position Title"
+                    value={positionTitle} onChange={handlePositionTitleChange} />
+
+                <label htmlFor="startDate">Start Date: </label>
+                <input type="text" name="startDate" id="startDate" placeholder="Enter Start Date"
+                    value={startDate} onChange={handleStartDateChange} />
+
+                <label htmlFor="endDate">End Date: </label>
+                <input type="text" name="endDate" id="endDate" placeholder="Enter End Date"
+                    value={endDate} onChange={handleEndDateChange} />
+
+                <label htmlFor="description">Description: </label>
+                <textarea type="text" name="description" id="description" placeholder="Description"
+                    value={description} onChange={handleDescriptionChange}></textarea>
+                <button type="submit">Add Experience</button>
+            </form>
+        </div>
+    )
+}
