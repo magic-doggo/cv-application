e042e054798c4ee6c8b5fa80ad624dd829f053d5
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-01-23 06:53:12 +0000

    Move Education to its own component and fix functionality it can now submit or delete education entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import PersonalDetails, { EducationalExperience} from './components/FormInput'
+import PersonalDetails from './components/FormInput'
 import { PracticalExperience } from './components/PracticalExperience'
+import { Education } from './components/Education'
 
 function App() {
   return (
     <>
       <PersonalDetails></PersonalDetails>
-      <EducationalExperience></EducationalExperience>
+      <Education></Education>
       <PracticalExperience></PracticalExperience>
     </>
   )
