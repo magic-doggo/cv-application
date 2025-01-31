41049b4b187f150ae8db123f2dab84fad2bf21a4
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-01-31 07:16:45 +0000

    Split Education into 2 different components for Form and output

--- src/components/CVInfo.jsx
+++ src/components/CVInfo.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { PracticalExperienceForm } from "./PracticalExperienceForm";
+import { EducationForm } from "./EducationForm";
 
 export function CVPage() {
     const [experienceEntries, setExperienceEntries] = useState([]);
+    const [educationEntries, setEducationEntries] = useState([]);
+
     const [nextId, setNextId] = useState(0);
 
 
@@ -11,10 +14,15 @@ export function CVPage() {
         setNextId(nextId + 1);
     }
 
+    function addEducation(entry) {
+        setEducationEntries([...educationEntries, {...entry, id: nextId}]);
+        setNextId(nextId + 1);
+    }
+
     return (
         <div>
             <PracticalExperienceForm addExperienceEntry={addExperience}></PracticalExperienceForm>
-            {console.log(experienceEntries)}
+            <EducationForm addEducationEntry={addEducation}></EducationForm>
             <ul>
                 {experienceEntries.map(experience => (
                     <li key={experience.id}>
@@ -29,6 +37,23 @@ export function CVPage() {
                                 )
                             )
                         }}>Delete {experience.companyName}</button>
+                    </li>
+                ))}
+            </ul>
+
+            <ul>
+                {educationEntries.map(education => (
+                    <li key={education.id}>
+                        <div>{education.school}</div>
+                        <div>{education.degree}</div>
+                        <div>{education.startDate} - {education.endDate}</div>
+                        <button onClick={() => {
+                            setEducationEntries(
+                                educationEntries.filter(edu =>
+                                    edu.id !== education.id
+                                )
+                            )
+                        }}>Delete {education.degree}</button>
                     </li>
                 ))}
             </ul>
