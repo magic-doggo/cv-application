3c58122fc11e4caa2ffcda4e4899449e2b3683ff
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-02-09 07:01:16 +0000

    Fix issue where value of form input cannot be less that 1 character or it resets to full previous entry

--- src/components/PracticalExperienceForm.jsx
+++ src/components/PracticalExperienceForm.jsx
@@ -8,21 +8,31 @@ export function PracticalExperienceForm({ addExperienceEntry, editExperience, is
     const [startDate, setStartDate] = useState(editExperience ? editExperience.startDate : '');
     const [endDate, setEndDate] = useState(editExperience ? editExperience.endDate : '');
     const [description, setDescription] = useState(editExperience ? editExperience.description : '');
+    const [editedCompanyName, setEditedCompanyName] = useState(false);
+    const [editedPositionTitle, setEditedPositionTitle] = useState(false);
+    const [editedStartDate, setEditedStartDate] = useState(false);
+    const [editedEndDate, setEditedEndDate] = useState(false);
+    const [editedDescription, setEditedDescription] = useState(false);
 
     function handleCompanyNameChange(e) {
-        setCompanyName(e.target.value)
+        setCompanyName(e.target.value);
+        setEditedCompanyName(true);
     }
     function handlePositionTitleChange(e) {
-        setPositionTitle(e.target.value)
+        setPositionTitle(e.target.value);
+        setEditedPositionTitle(true);
     }
     function handleStartDateChange(e) {
         setStartDate(e.target.value);
+        setEditedStartDate(true);
     }
     function handleEndDateChange(e) {
         setEndDate(e.target.value);
+        setEditedEndDate(true);
     }
     function handleDescriptionChange(e) {
         setDescription(e.target.value);
+        setEditedDescription(true);
     }
 
     function clearForm() {
@@ -54,6 +64,11 @@ export function PracticalExperienceForm({ addExperienceEntry, editExperience, is
             description
         });
         clearForm();
+        setEditedCompanyName(false);
+        setEditedPositionTitle(false);
+        setEditedStartDate(false);
+        setEditedEndDate(false);
+        setEditedDescription(false);
     }
 
     return (
@@ -61,30 +76,35 @@ export function PracticalExperienceForm({ addExperienceEntry, editExperience, is
             <label htmlFor="companyName">Company Name: </label>
             <input type="text" name="companyName" id="companyName" placeholder="Enter Company Name"
             onChange={handleCompanyNameChange}
-            value={editExperience ? (companyName == '' ? editExperience.companyName : companyName) : companyName} 
+            value={editExperience ? ((companyName == '' && editedCompanyName == false) ? (editExperience.companyName) : companyName) : companyName} 
             />
 
             <label htmlFor="positionTitle">Position Title: </label>
             <input type="text" name="positionTitle" id="positionTitle" placeholder="Enter Position Title"
             onChange={handlePositionTitleChange}
-            value={editExperience ? (positionTitle == '' ? editExperience.positionTitle : positionTitle) : positionTitle}  />
+            value={editExperience ? ((positionTitle == ''  && editedPositionTitle == false) ? editExperience.positionTitle : positionTitle) : positionTitle}  />
 
             <label htmlFor="startDate">Start Date: </label>
             <input type="text" name="startDate" id="startDate" placeholder="Enter Start Date"
             onChange={handleStartDateChange} 
-            value={editExperience ? (startDate == '' ? editExperience.startDate : startDate) : startDate} />
+            value={editExperience ? ((startDate == '' && editedStartDate == false) ? editExperience.startDate : startDate) : startDate} />
 
             <label htmlFor="endDate">End Date: </label>
             <input type="text" name="endDate" id="endDate" placeholder="Enter End Date"
             onChange={handleEndDateChange}
-            value={editExperience ? (endDate == '' ? editExperience.endDate : endDate) : endDate} />
+            value={editExperience ? ((endDate == '' && editedEndDate == false ) ? editExperience.endDate : endDate) : endDate} />
 
             <label htmlFor="description">Description: </label>
             <textarea type="text" name="description" id="description" placeholder="Description"
             onChange={handleDescriptionChange}
-            value={editExperience ? (description == '' ? editExperience.description : description) : description}>
+            value={editExperience ? ((description == '' && editedDescription == false) ? editExperience.description : description) : description}>
             </textarea>
             <button type="submit">{isCurrentlyEditing ? 'Submit changes' : 'Add Experience'}</button>
         </form>
     )
 }
+
+//only steps left are:
+//- set setEdited states to false when pressing edit buttons - right now it only works on submit
+//- onChange does not trigger the form "value" is changed automatically.
+//hence the state is not changing when editing an entry unless I modify each field. if I submit without editing a field, it will be empty
